cc5dc90095e7ac5877ea0935640c35b9fdded35e
Jiří Klimeš <jiri@klimes.dev>
2026-05-27 10:27:35 +0200

    iterative

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,13 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn fac(x: u64) -> u64 {
+    (1..=x).into_iter().product()
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::fac;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_fac_5() {
+        assert_eq!(fac(5), 120);
     }
 }
